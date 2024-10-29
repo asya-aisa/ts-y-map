47712311a120cc04276a99be05cb6c6d60c03073
asya-aisa <asyajujja@gmail.com>
2024-10-29 11:44:50 +0300

    test search

--- src/components/ui/admin-board/AdminBoard.tsx
+++ src/components/ui/admin-board/AdminBoard.tsx
@@ -1,10 +1,8 @@
-import { ChangeEvent, FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import '../../../App.scss'
-import { useActions } from '../../../hooks/useActions'
 import { useTypedSelector } from '../../../hooks/useTypesHooks'
 import { IChangeNewCoord } from '../../../store/polygon/polygon.interface'
 import { getPolygons } from '../../../store/polygon/polygon.slice'
-import SearchField from '../search-field/SearchField'
 import AddNewPolygon from './addNewPolygon/AddNewPolygon'
 import styles from './AdminBoard.module.scss'
 import AdminBoardItem from './AdminBoardItem'
@@ -22,44 +20,44 @@ const AdminBoard: FC<IAdminBoard> = ({
 }) => {
 	const polygons = useTypedSelector(getPolygons)
 	//const [inputValue, setInputValue] = useState<string>('')
-	const [searchTerm, setSearchTerm] = useState<string>('')
-	const [searchCoord, setSearchCoord] = useState<number[]>([])
+	// const [searchTerm, setSearchTerm] = useState<string>('')
+	// const [searchCoord, setSearchCoord] = useState<number[]>([])
 
-	const { toChangeMapState } = useActions()
+	// const { toChangeMapState } = useActions()
 
-	const API_KEY = '451f295a-dec2-4cc4-8b5d-99f9bf679e8d'
+	// const API_KEY = '451f295a-dec2-4cc4-8b5d-99f9bf679e8d'
 
-	useEffect(() => {
-		async function test() {
-			const res = await fetch(
-				`https://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY}&format=json&geocode=${searchTerm}`
-			)
-			const { response } = await res.json()
-			const newState =
-				response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
-			const coords = newState.split(' ').map(Number).reverse()
-			setSearchCoord(coords)
+	// useEffect(() => {
+	// 	async function test() {
+	// 		const res = await fetch(
+	// 			`https://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY}&format=json&geocode=${searchTerm}`
+	// 		)
+	// 		const { response } = await res.json()
+	// 		const newState =
+	// 			response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+	// 		const coords = newState.split(' ').map(Number).reverse()
+	// 		setSearchCoord(coords)
 
-			console.log(response)
-		}
+	// 		console.log(response)
+	// 	}
 
-		test()
-	}, [searchTerm])
+	// 	test()
+	// }, [searchTerm])
 
-	const handleSearch = () => {
-		toChangeMapState(searchCoord)
-	}
+	// const handleSearch = () => {
+	// 	toChangeMapState(searchCoord)
+	// }
 
 	return (
 		<div className={styles.adminBoardWrapper}>
-			<input
+			{/* <input
 				onChange={(e: ChangeEvent<HTMLInputElement>) =>
 					setSearchTerm(e.target.value)
 				}
-			/>
+			/> */}
 
-			<SearchField searchTerm={searchTerm} handleSearch={handleSearch} />
-			<button onClick={handleSearch}>search</button>
+			{/* <SearchField searchTerm={searchTerm} handleSearch={handleSearch} />
+			<button onClick={handleSearch}>search</button> */}
 			<h1>{header}</h1>
 			{polygons &&
 				polygons.map((el, index) => (
